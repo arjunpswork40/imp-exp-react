a50d0d6a3043e91a228f9f1f180901b504045128
arjunpswork40 <arjunpswork40@gmail.com>
2023-10-23 22:45:23 +0530

    useEffect added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Header.module.css'
 import Logo from '../../assets/logo.png'
 import {CgShoppingBag} from 'react-icons/cg'
@@ -11,6 +11,29 @@ const Header = () => {
     const toggleMenu = () => {
         setShowMenu((ShowMenu)=>!ShowMenu)
     }
+
+    useEffect(() => {
+        // Check screen width and set initial ShowMenu state
+        const handleWindowResize = () => {
+          if (window.innerWidth <= 640) {
+            setShowMenu(false); // Mobile screen
+          } else {
+            setShowMenu(true); // Larger screens (monitors)
+          }
+        };
+    
+        // Add event listener for window resize
+        window.addEventListener('resize', handleWindowResize);
+    
+        // Call the handler once to set the initial state
+        handleWindowResize();
+    
+        // Clean up the event listener when the component unmounts
+        return () => {
+          window.removeEventListener('resize', handleWindowResize);
+        };
+      }, []);
+    
 
     return (
         <div className={css.container}>
